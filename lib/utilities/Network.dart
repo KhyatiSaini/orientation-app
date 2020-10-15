8f707a8e42cc86b79976589acd17cd92552b018e
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-10-15 23:30:26 +0530

    Adding Http Package and creating a helper class to implement get request

---
+++ lib/utilities/Network.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+  String url;
+
+  Future<dynamic> GET() async {
+    http.Response response = await http.get(url);
+    Map resp;
+    var body = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      resp['body'] = body;
+      resp['status_code'] = response.statusCode;
+    } else {
+      resp['error'] = body;
+      resp['status_code'] = response.statusCode;
+      resp['message'] = "There is some error";
+    }
+    return resp;
+  }
+}
