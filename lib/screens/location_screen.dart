7e851fe923f4a70f6e6f21fb71000d4c1028ece0
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-10-23 22:58:58 +0530

    adding new screen

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class LocationScreen extends StatefulWidget {
+  static String route = "/location-screen";
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
